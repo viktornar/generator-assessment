7c9214a148f59500018ad9bd824c78d8883090a2
Viktor Nareiko <vnareiko.lt@gmail.com>
2020-03-02 08:09:00 +0200

    Removed redundant null keyword initialization.

--- src/main/java/org/bitbucket/viktornar/utils/CliUtils.java
+++ src/main/java/org/bitbucket/viktornar/utils/CliUtils.java
@@ -9,7 +9,7 @@ public class CliUtils {
     public static Map<String, String> parseArguments(String... args) {
         HashMap<String, String> arguments = new HashMap<>();
 
-        String key = null;
+        String key;
 
         for (int i = 0; i < args.length; i++) {
             if (args[i].startsWith("--")) {
